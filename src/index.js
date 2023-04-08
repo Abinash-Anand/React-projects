198e57958ce3366dad127b973e941957d1328c7c
Abinash Anand <74396889+Abinash-Anand@users.noreply.github.com>
2023-04-08 23:27:37 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>Harry You're a Wizard!</h1>
+    <ul>
+      <li>Harry Potter</li>
+      <li>Hermoine Granger</li>
+      <li>Ron Weasely</li>
+    </ul>
+  </div>,
+  document.querySelector("#root")
+);
